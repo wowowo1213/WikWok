44d225ec45b41cc2cdeb9c10b70ae8c5d271fbdd
wowowo <158035249+wowowo1213@users.noreply.github.com>
2025-11-22 15:48:14 +0800

    2025-11-22 修改

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline text-red-200">Hello world!</h1>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
